4e8e7223c222ac7a3102606647128885035b5198
Asepdadan <dadanasep74@gmail.com>
2016-01-09 21:04:36 +0700

    latihan ajax pertama

---
+++ action.php
@@ -0,0 +1,19 @@
+<?php
+
+if($_POST['nama'] && $_POST['alamat']){
+$var = $_POST['nama'];
+$var1 = $_POST['alamat'];
+
+}else{
+$var = '';
+
+}
+
+echo $var;
+echo $var1;
+
+
+//echo "hello ajax kenalin ajax";
+
+
+?>
---
+++ lihat.php
@@ -0,0 +1,13 @@
+<?php
+
+if($_POST['npm'] && $_POST['nama']){
+echo $_POST['npm']."<br>";
+echo $_POST['nama'];
+
+
+}else{
+
+	echo "data belum ada";
+}
+
+?>
